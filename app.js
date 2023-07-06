e4cd29beeba64f28ea83b125ce78baa01caedb7a
AldiMeirano <aldymerno173@gmail.com>
2023-07-05 18:10:49 -0700

    Add express ejs, AND Add listen port Route home in app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//jshint esversion:6
+const express = require('express');
+const app = express();
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended : true}))
+app.use(express.static('public'))
+
+
+app.get('/',(req,res) => { 
+    res.render('home')
+});
+
+app.route('/register')
+.get((req,res) => { 
+    res.render('register')
+})
+.post((req,res) => { 
+    console.log(req.body);
+})
+
+app.route('/login')
+.get((req,res) => { 
+    res.render('login')
+})
+
+app.listen(3000, () => { 
+    console.log('Server running in Port 3000');
+})
