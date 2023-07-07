20307a16271a995a043b33c3a5e32801e68823d7
AldiMeirano <aldymerno173@gmail.com>
2023-07-06 20:25:24 -0700

    Authentication and Security Database Level 2, add Env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const ejs = require('ejs')
@@ -9,6 +10,7 @@ const App = require('./dataAndCollect/dbAndCollect');
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
+
 
 
 app.get('/',(req,res) => { 
--- dataAndCollect/dbAndCollect.js
+++ dataAndCollect/dbAndCollect.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 mongoose.connect('mongodb://127.0.0.1:27017/appSecret');
@@ -6,8 +7,8 @@ const userSchema = new mongoose.Schema(
     { username : String, password : String }
 )
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ["password"]});
 
 const App = mongoose.model('secrets', userSchema);
 module.exports = App;
